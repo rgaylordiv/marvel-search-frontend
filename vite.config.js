773923ebbc69d8b77ac70c62bea25dd5bd4c96a6
Roger Gaylord IV <rgaylordiv@gmail.com>
2024-11-17 18:47:11 -0800

    base corrected

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/marvel-comic-search/",
+  base: "/marvel-search-frontend/",
   server: {
     port: 3001,
   },
