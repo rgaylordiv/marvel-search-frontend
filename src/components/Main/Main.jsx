addbadad7975b6d9019eb12448c89772680bef80
Roger Gaylord IV <rgaylordiv@gmail.com>
2024-12-05 01:35:55 -0800

    comic search working

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,7 +10,10 @@ export default function Main() {
         <Banner />
         <section className="main__profile-page">
           <Link to="/search" className="main-link">
-            <p className="main__search-page_text">Search our library now!</p>
+            <p className="main__search-page_text">Search the character library!</p>
+          </Link>
+          <Link to="/comic-search" className="main-link">
+            <p className="main__profile-page_text">Search the comic library!</p>
           </Link>
           {/* <Link to="/profile" className="main-link">
             <p className="main__profile-page_text">Find Your Favorite Items</p>
